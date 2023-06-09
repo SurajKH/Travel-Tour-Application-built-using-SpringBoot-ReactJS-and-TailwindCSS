d204f93e948302fe883c3ce2134e9a9c1136887b
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-06-09 11:14:10 +0530

    Add files via upload

---
+++ TravelApplication/repository/TourManagerRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourManagerRepository extends JpaRepository<TourManagerRepository,Long> {
+
+}
---
+++ TravelApplication/repository/TouristRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TouristRepository extends JpaRepository<TouristRepository,Long> {
+
+}
