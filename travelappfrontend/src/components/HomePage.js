90e6a855040003d5eb5cca5fb93fa4202d42c275
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-06-10 09:45:36 +0530

    Add files via upload

---
+++ travelappfrontend/src/components/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import image from '../assets/travelpic.jpg';
+import AdminPage from './AdminPage';
+
+
+//Home Page Component.
+const HomePage = () => {
+  return (
+    <React.Fragment>
+       <div style={{backgroundImage:
+      `url(${image})`,backgroundPosition: 'center',
+backgroundSize:'cover',backgroundRepeat: 'no-repeat',
+width: '100vw',
+        height: '100vh'
+ }}>
+ {/*
+ Designing of Admin Page using the flex property.
+ */}
+ <AdminPage/>
+       </div>
+    </React.Fragment>
+  )
+}
+
+export default HomePage;
