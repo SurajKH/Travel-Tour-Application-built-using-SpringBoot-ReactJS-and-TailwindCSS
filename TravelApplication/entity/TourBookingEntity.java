6f089ddb6c2f420f84d07479c119f57f2f870842
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-06-11 12:08:26 +0530

    Update TourBookingEntity.java

--- TravelApplication/entity/TourBookingEntity.java
+++ TravelApplication/entity/TourBookingEntity.java
@@ -25,50 +25,17 @@ import lombok.NoArgsConstructor;
 @Table(name="tour_booking_table")
 public class TourBookingEntity {
 
-	//lets consider a tour booking Entity.
-	//TourBookingID
-	//tourManager
-	//PackageType
-	//Number of Days
-	
-	//Discount based on Total.
-	//Payment Details--->we need to generate it and store it in the file..
-	
-	//then we can generate a summary and store it in the text file.
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long tourbookingid;
 	
     private String packagetype;
 
-	private String tourmentorating;
+    private String tourmentorating;
     
     private String tourmentoryoe;
     
-    
-    
-    //private Long TourManagerReferenceID;
-    
-    //concept of Aggregation.
-//    @OneToOne(cascade = CascadeType.ALL)
-//    @JoinColumn(name="TourManagerReferenceID",referencedColumnName="Tourmanagerid")
-//    private TourManagerEntity tourmanager;
-    
-    
-    //lets consider the tourmanager being grouped with the concept of 
-    //tourmanager YOE and Ratings.
-	
-
-//	public TourManagerEntity getTourmanager() {
-//		return tourmanager;
-//	}
-//
-//	public void setTourmanager(TourManagerEntity tourmanager)
-//	{
-//	 
-//		this.tourmanager = tourmanager;
-//     }
-
+ 
 	public Long getTourbookingid() {
 		return tourbookingid;
 	}
