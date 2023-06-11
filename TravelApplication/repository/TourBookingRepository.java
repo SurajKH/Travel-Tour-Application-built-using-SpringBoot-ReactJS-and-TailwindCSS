4b9e9beec54cfd2e8340999b86ee5407055feb3b
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-06-11 12:12:20 +0530

    Update TourBookingRepository.java

--- TravelApplication/repository/TourBookingRepository.java
+++ TravelApplication/repository/TourBookingRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.TourBookingEntity;
 
+@Repository
 public interface TourBookingRepository extends JpaRepository<TourBookingEntity,Long> {
 
 }
