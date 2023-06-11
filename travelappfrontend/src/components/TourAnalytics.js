aa0dd4ed5cb5ec408f8261bcb5ee28bd8b5ad62f
surajkh <90398336+SurajKH@users.noreply.github.com>
2023-06-11 13:33:55 +0530

    Add files via upload

---
+++ travelappfrontend/src/components/TourAnalytics.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//we are considering the Tour Analytics Component over here.
+
+
+const TourAnalytics = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TourAnalytics;
